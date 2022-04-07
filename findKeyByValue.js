8348405f7f2df40a6e7599b74c8be8764d076438
Thushara <thusharan@hotmail.com>
2022-04-06 20:39:45 -0400

    similar to findKey

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😃😃😃 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😭😭😭 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  const keysArray = Object.keys(object);
+  for (let element of keysArray) {
+    if (object[element] === value) return element;
+  }
+};
+ 
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
